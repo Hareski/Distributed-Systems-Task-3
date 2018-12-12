9f0945d74118c32c8ca86dc331d9103aa5a65fb5
Areski Guilhem Himeur <areski.himeur@gmail.com>
2018-12-12 15:22:49 +0200

    Upload

--- python_server/script.py
+++ python_server/script.py
@@ -8,6 +8,7 @@ import time
 import matplotlib.pyplot as plt
 import numpy as np
 
+
 def connect():
     push_service = FCMNotification(api_key=raw_input('Insert your Firebase Cloud Messaging Server key: '))
     registration_id = raw_input('Insert the instance ID Token of client: ')
@@ -44,7 +45,8 @@ def get_inter_arrival_rate(push, id, payload=200):
 
 
 def create_message(num_chars):
-    message_body = "".join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(num_chars))
+    message_body = "".join(
+        random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(num_chars))
     return message_body
 
 
@@ -82,26 +84,25 @@ def send_messages_with_plot(push_service, registration_id, num_messages=50, size
 
 if __name__ == '__main__':
     push_service, registration_id = connect()
-    # push_service = FCMNotification(api_key="AAAAU8Vo7C4:APA91bHpu-T-CArTfaTjMQ0CeCNlZ2c81afB2SeFtGyouutvUt7QURvbjhzQkZfdsul_ToIvWleH2ZhTtnAp_X4R0n9htiUqlvzZTHbks2boxtl6VRUOtu3ET0Q4X8mJpxv8xLEW4MUg")
-    # registration_id = "d0B81WTvzqU:APA91bG97v-r4vHgzlSOPFI2Mid0rH3io4l-bLTAgYtQDjKeTyu4fkLdiatA7Bj-xz9qqT69CSe5zC6OZEcOtQnMwB8o_-XGZefO2fiffWKPomofq3ujWrkKENj2xAZbwAnEqe7E7d4G"
 
-    # exercise 1
-    # print("Average time to send 50 random messages: %s seconds." % send_50times_rand_payload(push_service, registration_id))
+    print("<-- exercise 1 -->")
+    print("Average time to send 50 random messages: %s seconds." % send_50times_rand_payload(push_service,
+                                                                                             registration_id))
 
-    # exercise 2
-    # time_list = []
-    #
-    # mess_size = 2
-    #
-    # for i in range(0, 10):
-    #     print("------ITERATION NUMBER {} -------- ".format(str(i+1)))
-    #     delta = send_fixed_messages(push_service, registration_id, mess_size, 25)
-    #     time_list.append(delta)
-    #
-    # print("Average time for 25 messages of size {}:".format(mess_size), (sum(time_list) / float(len(time_list))))
+    print("<-- exercise 2 -->")
+    time_list = []
 
-    # exercise 3
-    # print("Inter arrival date of average message payload: %s seconds." % get_inter_arrival_rate(push_service, registration_id))
+    mess_size = 2
 
-    # exercise 4
+    for i in range(0, 10):
+        print("------ITERATION NUMBER {} -------- ".format(str(i + 1)))
+        delta = send_fixed_messages(push_service, registration_id, mess_size, 25)
+        time_list.append(delta)
+
+    print("Average time for 25 messages of size {}:".format(mess_size), (sum(time_list) / float(len(time_list))))
+
+    print("<-- exercise 3 -->")
+    print("Inter arrival date of average message payload: %s seconds." % get_inter_arrival_rate(push_service, registration_id))
+
+    print("<-- exercise 4 -->")
     send_messages_with_plot(push_service, registration_id, num_messages=200)
